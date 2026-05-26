5ebee5b3b011eae69075d04e349270449f24d132
VuLkAn911 <donkikon828@gmail.com>
2026-05-26 19:32:10 +0500

    subcalculatelogistic

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	const BaseRate = 5.50
+	const TaxRate = 0.12
+	const DistanceRate = 2.0
+	const FragileFee = 0.2
+
+	fmt.Print("Введите имя:")
+	var name string
+	fmt.Scan(&name)
+
+	fmt.Print("Вес груза(Кг):")
+	var weight float64
+	fmt.Scan(&weight)
+
+	fmt.Print("Дистанция(км):")
+	var Distance float64
+	fmt.Scan(&Distance)
+
+	fmt.Print("Хрупких упаковок:")
+	var Fragile float64
+	fmt.Scan(&Fragile)
+
+	BaseRate2 := (weight*BaseRate)*(1+FragileFee*Fragile) + (Distance * DistanceRate)
+	total := BaseRate2 + TaxRate
+
+	fmt.Println("---Отчет о доставке---")
+	fmt.Printf(" Отправитель:%s\n Итоговая стоимость: %.2f\n", name, total)
+
+}
